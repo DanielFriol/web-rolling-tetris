2be403198aeefa37f81fc504ab30bdafa06b1ac4
Vitor Costa Batista <56313345+vitorczz1@users.noreply.github.com>
2020-11-24 21:33:53 -0300

    Update jogo.js

--- assets/javascript/jogo.js
+++ assets/javascript/jogo.js
@@ -11,3 +11,18 @@ function stop() {
     document.getElementById('play').style.display = 'initial';
     document.getElementById('stop').style.display = 'none';
 }
+
+function Timer() {
+
+var segundos = 0;
+var minutos = 0;
+
+setInterval(function(){
+  segundos++;
+    if (segundos > 59){ /*Se segundos forem maiores que 59*/
+     segundos = 0; /*Zera os segundos*/ 
+     minutos++; /*e Incrementa os minutos*/
+  }
+    document.getElementById("tempo").innerHTML = minutos + "m" + " : " + segundos + "s";
+ }, 1000);
+}
