d0455f426c7c2568fa2bcda26f604a8f335b493d
Vitor Costa Batista <56313345+vitorczz1@users.noreply.github.com>
2020-12-16 17:17:45 -0300

    Inicio do login

--- index.php
+++ index.php
@@ -10,6 +10,27 @@
   </head>
 
   <body>
+     <div>
+      <?php
+      session_start();
+      require_once('./server/dbconn.php');
+      ?>
+
+      <?php
+      if (isset($_POST['btnEntrar'])) {
+        $userName = $_POST['name'];
+        $password = $_POST['password'];
+
+       $query = "SELECT * FROM user WHERE userName ='$userName' AND password = '$password'";
+       $result = $queryinrt->exec($query);
+       
+       /*echo"<script language='javascript' type='text/javascript'>
+               window.location.href='jogo.html';
+             </script>";*/
+      }
+      ?>
+    </div>
+    
     <div class="principal">
       <h1>Entre para Jogar</h1>
       <div class="forms">
@@ -30,9 +51,9 @@
             </tbody>
           </table>
 
-          <button type="submit" class="mt-2">Entrar</button>
+          <button type="submit" name = "btnEntrar" class="mt-2">Entrar</button>
 
-          <p>Não possui cadastro? <a href="Cadastro.html"> Cadastre-se</a>!</p>
+          <p>Não possui cadastro? <a href="Cadastro.php"> Cadastre-se</a>!</p>
         </form>
       </div>
     </div>
