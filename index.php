b4ed30e8b4f8ca9a2babbed949b8aae71982cd5f
Vitor Costa Batista <56313345+vitorczz1@users.noreply.github.com>
2020-12-16 17:53:59 -0300

    Inicio do login

--- index.php
+++ index.php
@@ -23,9 +23,18 @@
 
        $query = "SELECT * FROM user WHERE userName ='$userName' AND password = '$password'";
        $result = $queryinrt->exec($query);
+        
+       $num_rows = mysql_num_rows($result);
        
+       if ($num_rows == 1){ 
+        
        /*echo"<script language='javascript' type='text/javascript'>
                window.location.href='jogo.html';
+             </script>";*/
+       }
+      }else{
+        /*echo"<script language='javascript' type='text/javascript'>
+               alert ('Usuário ou Senha incorreta !');
              </script>";*/
       }
       ?>
