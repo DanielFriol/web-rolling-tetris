d43aed59953c180bce57bc90d38a755a6986cf2a
Vitor Costa Batista <56313345+vitorczz1@users.noreply.github.com>
2020-12-17 20:34:34 -0300

    login inicial ok

--- server/index.php
+++ server/index.php
@@ -1,24 +1,45 @@
 <?php
-  require_once('./server/dbconn.php');
+  session_start();
+  include('dbconn.php');
 
     if (isset($_POST['btnEntrar'])) {
       $userName = $_POST['name'];
       $password = $_POST['password'];
 
-      $query = "SELECT * FROM user WHERE userName ='$userName' AND password = '$password'";
-      $result = $queryinrt->exec($query);
+      $query = "select * from user where userName ='$userName' and password = '$password'";
+      $result = mysqli_query($conexao, $query);
         
-      $num_rows = mysql_num_rows($result);
+      $num_rows = mysqli_num_rows($result);
        
     if ($num_rows == 1){ 
         
-       /*echo"<script language='javascript' type='text/javascript'>
-               window.location.href='jogo.html';
-             </script>";*/
-       }
+       echo"<script language='javascript' type='text/javascript'>
+              alert('Login realizado com sucesso !');
+              window.location.href='../jogo.html';
+            </script>";
+
+    }else if ((empty($userName) || $userName == null) && (empty($password) || $password == null)){
+        echo"<script language='javascript' type='text/javascript'>
+              alert ('Falta preencher o Nome de Usuário e Senha !');
+              window.location.href='../index.html';
+            </script>";
+
+    }else if (empty($userName) || $userName == null){
+        echo"<script language='javascript' type='text/javascript'>
+               alert ('Falta preencher o Nome de Usuário !');
+               window.location.href='../index.html';
+             </script>";
+
+    }else if (empty($password) || $password == null){
+        echo"<script language='javascript' type='text/javascript'>
+               alert ('Falta preencher a Senha !');
+               window.location.href='../index.html';
+             </script>";
+
     }else{
-        /*echo"<script language='javascript' type='text/javascript'>
+        echo"<script language='javascript' type='text/javascript'>
                alert ('Usuário ou Senha incorreta !');
-             </script>";*/
-    }
+               window.location.href='../index.html';
+             </script>";
+    }}
 ?>
